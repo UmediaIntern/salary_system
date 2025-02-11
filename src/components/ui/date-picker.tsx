48392b929ae023a0907b6a761ec51b6815e7016d
Ethan-Chiu <ethanchiu0608@gmail.com>
2025-02-11 16:25:51 +0800

    Date picker with input field.

--- src/components/ui/date-picker.tsx
+++ src/components/ui/date-picker.tsx
@@ -1,7 +1,4 @@
-"use client";
-import { format } from "date-fns";
 import { Calendar as CalendarIcon } from "lucide-react";
-
 import { cn } from "~/lib/utils";
 import { Button } from "~/components/ui/button";
 import { Calendar } from "~/components/ui/calendar";
@@ -10,42 +7,60 @@ import {
 	PopoverContent,
 	PopoverTrigger,
 } from "~/components/ui/popover";
-import { forwardRef } from "react";
+import { DateTimeInput } from "./date-time-input";
 
-const DatePicker = forwardRef<
-	HTMLDivElement,
-	{
-		date?: Date;
-		setDate: (date?: Date) => void;
-	} & React.HTMLAttributes<HTMLDivElement>
->(({ className, date, setDate, ...props }, ref) => (
-	<Popover {...props} modal>
-		<PopoverTrigger asChild>
-			<Button
-				variant={"outline"}
-				className={cn(
-					"w-full justify-start text-left font-normal",
-					!date && "text-muted-foreground"
-				)}
-			>
-				<CalendarIcon className="mr-2 h-4 w-4" />
-				{date ? format(date, "PPP") : <span>Pick a date</span>}
-			</Button>
-		</PopoverTrigger>
-		<PopoverContent className={cn("w-auto p-0", className)} ref={ref}>
-			<Calendar
-				mode="single"
-				selected={date}
-				onSelect={setDate}
-				fixedWeeks
-				captionLayout="dropdown-buttons"
-				fromYear={new Date().getFullYear() - 30}
-				toYear={new Date().getFullYear() + 30}
-			/>
-		</PopoverContent>
-	</Popover>
-));
 
-DatePicker.displayName = "DatePicker";
-
-export { DatePicker };
+export function DatePicker({
+	className,
+	date,
+	setDate,
+	...props
+}: {
+	date?: Date;
+	setDate: (date?: Date) => void;
+} & React.HTMLAttributes<HTMLDivElement>) {
+	return (
+		<Popover {...props} modal>
+			<div className="relative w-full">
+				<DateTimeInput
+					hideCalendarIcon
+					value={date}
+					onChange={(x) => setDate(x)}
+					format="yyyy/MM/dd"
+				/>
+				<PopoverTrigger asChild>
+					<Button
+						variant={"outline"}
+						className={cn(
+							"absolute right-0 top-[50%] translate-y-[-50%] rounded-l-none font-normal",
+							!date && "text-muted-foreground"
+						)}
+					>
+						<CalendarIcon className="h-4 w-4" />
+					</Button>
+				</PopoverTrigger>
+				{/* 	<Button */}
+				{/* 		variant={"outline"} */}
+				{/* 		className={cn( */}
+				{/* 			"w-full justify-start text-left font-normal", */}
+				{/* 			!date && "text-muted-foreground" */}
+				{/* 		)} */}
+				{/* 	> */}
+				{/* 		<CalendarIcon className="mr-2 h-4 w-4" /> */}
+				{/* 	</Button> */}
+			</div>
+			<PopoverContent className={cn("w-auto p-0", className)}>
+				<Calendar
+					mode="single"
+					selected={date}
+					onSelect={setDate}
+					defaultMonth={date}
+					fixedWeeks
+					captionLayout="dropdown-buttons"
+					fromYear={new Date().getFullYear() - 30}
+					toYear={new Date().getFullYear() + 30}
+				/>
+			</PopoverContent>
+		</Popover>
+	);
+}
