501fef5006c2a8c71d8fc187121af685262b2525
Ethan-Chiu <ethanchiu0608@gmail.com>
2025-06-24 14:02:36 +0800

    Update trpc version. Interupt upload if transaction already exist.

--- src/pages/import/validate_excel.tsx
+++ src/pages/import/validate_excel.tsx
@@ -8,20 +8,51 @@ import { api } from "~/utils/api";
 import { useImportContext } from "./import_context_provider";
 import { ImportPreview } from "./import_preview";
 import { useState } from "react";
+import { onPromise } from "~/utils/on_promise";
 
 const fields = ["a", "b", "c"];
 
 export function ValidateExcel() {
-	const [inputPeriod, setInputPeriod] = useState(null);
+	const [isUploading, setIsUploading] = useState(false);
+	const [openDialog, setOpenDialog] = useState(false);
+	const trpcUtils = api.useUtils();
 
 	const importTransaction =
 		api.importTransaction.importTransaction.useMutation();
 
 	const { excelData } = useImportContext();
 
-	function handleUpload() {
-		console.log(excelData);
-		importTransaction.mutate(excelData, { onSuccess: () => {} });
+	async function handleUpload() {
+		setIsUploading(true);
+
+		// TODO: Improve. Try to find the period id
+		const period_id = excelData.at(0)?.period_id;
+		if (period_id == null) {
+			console.error("Missing period_id");
+			setIsUploading(false);
+			return;
+		}
+
+		try {
+			const { empty } =
+				await trpcUtils.importTransaction.checkImportTransaction.fetch({
+					period_id: period_id,
+				});
+
+			if (empty) {
+				console.log(excelData);
+				importTransaction.mutate(excelData, {
+					onSuccess: () => setIsUploading(false),
+					onError: () => setIsUploading(false),
+				});
+			} else {
+				setOpenDialog(true);
+				setIsUploading(false);
+			}
+		} catch (err) {
+			console.error("checkImportTransaction failed:", err);
+			setIsUploading(false);
+		}
 	}
 
 	return (
@@ -55,10 +86,9 @@ export function ValidateExcel() {
 				</ResizablePanel>
 			</ResizablePanelGroup>
 			<Button
+				disabled={isUploading}
 				className="absolute bottom-4 right-4"
-				onClick={() => {
-					handleUpload();
-				}}
+				onClick={onPromise(handleUpload)}
 			>
 				upload
 			</Button>
