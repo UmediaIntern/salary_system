ad9c98ba1a8ff5d0f9e315a1e7df77080240fd1d
Ethan-Chiu <ethanchiu0608@gmail.com>
2025-06-24 15:46:29 +0800

    Delete existing transaction dialog.

--- src/pages/import/validate_excel.tsx
+++ src/pages/import/validate_excel.tsx
@@ -9,6 +9,17 @@ import { useImportContext } from "./import_context_provider";
 import { ImportPreview } from "./import_preview";
 import { useState } from "react";
 import { onPromise } from "~/utils/on_promise";
+import { toast } from "sonner"
+import {
+	Dialog,
+	DialogClose,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "~/components/ui/dialog";
 
 const fields = ["a", "b", "c"];
 
@@ -55,43 +66,96 @@ export function ValidateExcel() {
 		}
 	}
 
+	async function handleDelete() {
+		toast("Continue to delete?", {
+			description: "Confirm to delete",
+			action: {
+				label: "Confirm",
+				onClick: () => console.log("Delete"),
+			},
+		});
+	}
+
 	return (
 		<div className="relative h-full w-full">
-			<ResizablePanelGroup
-				direction="vertical"
-				className="flex h-full w-full flex-col"
-			>
-				<ResizablePanel defaultSize={50}>
-					<div className="h-full w-full">
-						<ImportPreview />
-					</div>
-				</ResizablePanel>
-				<ResizableHandle />
+			<Dialog open={openDialog}>
+				<ResizablePanelGroup
+					direction="vertical"
+					className="flex h-full w-full flex-col"
+				>
+					<ResizablePanel defaultSize={50}>
+						<div className="h-full w-full">
+							<ImportPreview />
+						</div>
+					</ResizablePanel>
+					<ResizableHandle />
 
-				<ResizablePanel defaultSize={50}>
-					{/* Missing fields */}
-					{/* Invalid values */}
-					<div className="flex h-full w-full flex-col bg-muted p-4">
-						{fields.map((field) => (
-							<div key={field}>
-								<Button
-									className="h-6 w-36"
-									variant="destructive"
-								>
-									{field}
-								</Button>
-							</div>
-						))}
-					</div>
-				</ResizablePanel>
-			</ResizablePanelGroup>
-			<Button
-				disabled={isUploading}
-				className="absolute bottom-4 right-4"
-				onClick={onPromise(handleUpload)}
-			>
-				upload
-			</Button>
+					<ResizablePanel defaultSize={50}>
+						{/* Missing fields */}
+						{/* Invalid values */}
+						<div className="flex h-full w-full flex-col bg-muted p-4">
+							{fields.map((field) => (
+								<div key={field}>
+									<Button
+										className="h-6 w-36"
+										variant="destructive"
+									>
+										{field}
+									</Button>
+								</div>
+							))}
+						</div>
+					</ResizablePanel>
+				</ResizablePanelGroup>
+				<Button
+					disabled={isUploading}
+					className="absolute bottom-4 right-4"
+					onClick={onPromise(handleUpload)}
+				>
+					upload
+				</Button>
+				<DeleteTransactionDialog
+					onClose={() => {
+						setOpenDialog(false);
+					}}
+					onSubmit={onPromise(handleDelete)}
+				/>
+			</Dialog>
 		</div>
+	);
+}
+
+interface DeleteTransactionDialogProps {
+	onClose: () => void;
+	onSubmit: () => void;
+}
+
+function DeleteTransactionDialog({
+	onClose,
+	onSubmit,
+}: DeleteTransactionDialogProps) {
+	return (
+		<DialogContent className="sm:max-w-[425px] [&>button]:hidden">
+			<DialogHeader>
+				<DialogTitle>Found existing transaction</DialogTitle>
+				<DialogDescription>
+					You are trying to upload a transaction that has the same
+					period as an existing transaction. Do you want to overwrite
+					the existing transaction? If you click continue, the
+					existing transaction and related data will be deleted.
+				</DialogDescription>
+			</DialogHeader>
+			<div className="grid gap-4"></div>
+			<DialogFooter>
+				<DialogClose asChild>
+					<Button variant="outline" onClick={onClose}>
+						Cancel
+					</Button>
+				</DialogClose>
+				<Button type="submit" onClick={onSubmit}>
+					Continue
+				</Button>
+			</DialogFooter>
+		</DialogContent>
 	);
 }
