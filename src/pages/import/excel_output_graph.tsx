66a3e84a5f73ad051b4e1f345302f10005c5cfee
Ethan-Chiu <ethanchiu0608@gmail.com>
2025-07-15 16:00:55 +0800

    Basic graph view.

--- src/pages/import/excel_output_graph.tsx
+++ src/pages/import/excel_output_graph.tsx
@@ -1,4 +1,5 @@
-import { ReactFlow, Background, Controls } from '@xyflow/react';
+import { ReactFlow, Background, Controls, applyNodeChanges, applyEdgeChanges, Edge, Node, NodeChange, EdgeChange, addEdge } from '@xyflow/react';
+import { useCallback, useState } from 'react';
 import { Drawer } from 'vaul';
 import { Button } from '~/components/ui/button';
 
@@ -19,10 +20,7 @@ export function ExcelOutputGraph() {
 				<DrawerDescription> Edit the excel data mapping</DrawerDescription>
 			</DrawerHeader>
 			<div className='h-full w-full'>
-				<ReactFlow>
-					<Background />
-					<Controls />
-				</ReactFlow>
+				<GraphView />
 			</div>
 			<DrawerFooter className="flex flex-row justify-end">
 				<Button>Submit</Button>
@@ -31,5 +29,52 @@ export function ExcelOutputGraph() {
 				</DrawerClose>
 			</DrawerFooter>
 		</DrawerContent>
+	)
+}
+
+const initialNodes = [
+	{
+		id: 'n1',
+		position: { x: 0, y: 0 },
+		data: { label: 'Node 1' },
+		type: 'input',
+	},
+	{
+		id: 'n2',
+		position: { x: 100, y: 100 },
+		data: { label: 'Node 2' },
+	},
+];
+
+function GraphView() {
+	const [nodes, setNodes] = useState<Node[]>(initialNodes);
+	const [edges, setEdges] = useState<Edge[]>([]);
+
+	const onNodesChange = useCallback(
+		(changes: NodeChange<Node>[]) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+		[],
+	);
+	const onEdgesChange = useCallback(
+		(changes: EdgeChange<Edge>[]) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+		[],
+	);
+
+	const onConnect = useCallback(
+		(params: any) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+		[],
+	);
+
+	return (
+		<ReactFlow
+			nodes={nodes}
+			edges={edges}
+			onNodesChange={onNodesChange}
+			onEdgesChange={onEdgesChange}
+			onConnect={onConnect}
+			fitView
+		>
+			<Background />
+			<Controls />
+		</ReactFlow>
 	)
 }
