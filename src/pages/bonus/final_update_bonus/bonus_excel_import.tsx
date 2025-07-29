e5abd9f55d668aa6f6512ecc3164d0f946908218
Ethan-Chiu <ethanchiu0608@gmail.com>
2025-07-29 15:54:25 +0800

    Clean up bonus code.

--- src/pages/bonus/final_update_bonus/bonus_excel_import.tsx
+++ src/pages/bonus/final_update_bonus/bonus_excel_import.tsx
@@ -1,34 +1,12 @@
 /* ShadCN UI */
-import { Button } from "~/components/ui/button";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
-import {
-	Dialog,
-	DialogContent,
-	DialogDescription,
-	DialogHeader,
-	DialogTitle,
-} from "~/components/ui/dialog";
-
-import {
-	Select,
-	SelectContent,
-	SelectGroup,
-	SelectItem,
-	SelectLabel,
-	SelectTrigger,
-	SelectValue,
-} from "~/components/ui/select";
 import { api } from "~/utils/api";
 import { useEffect, useState } from "react";
-import { FileUploader } from "~/components/file_operations/file_uploader";
-import { extractData } from "~/components/file_operations/excel_upload_utils";
+import { ExcelUpload } from "~/components/file_operations/excel_upload";
 
 export default function BonusExcelImport() {
-	const [view, setView] = useState<string>("upload");
 	const [selectedFile, setSelectedFile] = useState<string>("");
-	const [files, setFiles] = useState<File[]>([]);
 	const [datas, setDatas] = useState<Array<{ name: string; data: any[][] }>>(
-		[]
+		[],
 	);
 
 	const [errorEmployees, setErrorEmployees] = useState<Array<string>>([]);
@@ -47,37 +25,6 @@ export default function BonusExcelImport() {
 	});
 	const confirmUpdateFromExcel =
 		api.bonus.confirmUpdateFromExcel.useMutation();
-
-	async function handleFileUpload(acceptedFiles: File[]) {
-		setFiles(acceptedFiles);
-		acceptedFiles.sort((a: File, b: File) => a.name.localeCompare(b.name));
-
-		await Promise.all(
-			acceptedFiles.map(async (file) => {
-				// Record<string, any[][]>
-				const sheetsData = await extractData(file);
-				if (!sheetsData) {
-					return;
-				}
-				const sheet_names = Object.keys(sheetsData);
-				if (sheet_names[0]) {
-					const firstSheet = sheetsData[sheet_names[0]];
-					if (firstSheet) {
-						datas.push({
-							name: file.name,
-							data: firstSheet ?? [],
-						});
-					}
-				}
-			})
-		);
-
-		if (acceptedFiles[0]) {
-			setSelectedFile(acceptedFiles[0].name);
-		}
-		setDatas(datas);
-		setView("preview");
-	}
 
 	const selectedFileIdx = () =>
 		datas.findIndex((d) => d.name === selectedFile);
@@ -103,139 +50,11 @@ export default function BonusExcelImport() {
 		});
 	}
 
-	return (
-		<div className="grow flex-col w-full">
-			<Tabs
-				className="h-full w-full flex flex-col"
-				value={view}
-				onValueChange={(v) => setView(v)}
-			>
-				<TabsList className="grid w-full grid-cols-2 mb-2">
-					<TabsTrigger value="upload">Upload</TabsTrigger>
-					<TabsTrigger value="preview" disabled={files.length == 0}>
-						Preview
-					</TabsTrigger>
-				</TabsList>
-				<TabsContent value="upload" className="my-2 grow w-full">
-					<FileUploader onUpload={handleFileUpload} className="h-full"/>
-				</TabsContent>
-				<TabsContent value="preview" className="flex flex-col grow min-h-0">
-					<div className="mb-2 flex w-full items-center justify-between">
-						<Select
-							value={selectedFile}
-							onValueChange={setSelectedFile}
-						>
-							<SelectTrigger className="w-[180px]">
-								<SelectValue placeholder="Select a fruit" />
-							</SelectTrigger>
-							<SelectContent>
-								<SelectGroup>
-									<SelectLabel>Filename</SelectLabel>
-									{files.map((f) => (
-										<SelectItem key={f.name} value={f.name}>
-											{f.name}
-										</SelectItem>
-									))}
-								</SelectGroup>
-							</SelectContent>
-						</Select>
-
-						<Dialog
-							open={errorEmployees.length != 0}
-							onOpenChange={() => setErrorEmployees([])}
-						>
-							<DialogContent>
-								<DialogHeader>
-									<DialogTitle>
-										Are you absolutely sure to update bonus
-										table?
-									</DialogTitle>
-									<DialogDescription></DialogDescription>
-								</DialogHeader>
-								These are the bug employees
-								{errorEmployees.map((emp) => {
-									return (
-										<>
-											<p>{emp}</p>
-										</>
-									);
-								})}
-								<Button
-									variant={"outline"}
-									onClick={() => {
-										datas.map(async (data: any) => {
-											confirmUpdateFromExcel.mutate(
-												recoverObject(
-													data!.data as any
-												) as any
-											);
-										});
-										setErrorEmployees([]);
-									}}
-								>
-									Update
-								</Button>
-							</DialogContent>
-						</Dialog>
-						<Button
-							variant={"outline"}
-							onClick={() => {
-								datas.map(async (data: any) => {
-									updateFromExcel.mutate(
-										recoverObject(data!.data as any) as any
-									);
-								});
-							}}
-							className=""
-						>
-							Update
-						</Button>
-					</div>
-
-					<div className="flex grow overflow-auto border border-gray-300">
-						<table className="w-full border-collapse">
-							<thead>
-								<tr>
-									{/* Assuming the first row contains headers */}
-									{datas[selectedFileIdx()] &&
-										(
-											datas[selectedFileIdx()]
-												?.data?.[0] ?? []
-										).map((header: any, index: number) => (
-											<th
-												key={index}
-												className="border border-gray-300 p-2"
-											>
-												{header}
-											</th>
-										))}
-								</tr>
-							</thead>
-							<tbody>
-								{datas[selectedFileIdx()]?.data
-									.slice(1)
-									.map((row: any[], rowIndex: number) => (
-										<tr key={rowIndex}>
-											{row.map(
-												(
-													cell: any,
-													cellIndex: number
-												) => (
-													<td
-														key={cellIndex}
-														className="border border-gray-300 p-2"
-													>
-														{cell}
-													</td>
-												)
-											)}
-										</tr>
-									))}
-							</tbody>
-						</table>
-					</div>
-				</TabsContent>
-			</Tabs>
-		</div>
-	);
+	return <ExcelUpload onClick={(data) => {
+				console.log("data", data);
+				// Multi sheet: sheet_name -> Obj[]
+				// Single sheet: Obj[]
+				if (Array.isArray(data)) {
+				}
+			}}/>;
 }
--- src/pages/bonus/pre_calculate_bonus/bonus_excel_export.tsx
+++ src/pages/bonus/pre_calculate_bonus/bonus_excel_export.tsx
@@ -1,6 +1,63 @@
-import { BonusExcelExport as BonusExcelExportComponent} from "./excel_export/BonusExcelExport";
+import { useContext, useState } from "react";
+import dataTableContext from "../components/context/data_table_context";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { i18n, locales } from "~/components/lang_config";
+import { api } from "~/utils/api";
+import { usePeriodContext } from "~/components/context/period_context_provider";
+import { useQueryHandle } from "~/components/query_boundary/query_handle";
+import ExcelViewer from "./excel_export/ExcelViewer";
+import {
+	getExcelData,
+	getBonusExcel,
+	excludeDataColumn,
+} from "./excel_export/utils";
+
 export default function BonusExcelExport() {
-    return (
-            <BonusExcelExportComponent />
-    );
+	const { selectedPeriod } = usePeriodContext();
+	const { selectedBonusType } = useContext(dataTableContext);
+
+	const getExcelA = api.bonus.getExcelEmployeeBonus.useQuery({
+		period_id: selectedPeriod?.period_id ?? 0,
+		bonus_type: selectedBonusType,
+	});
+	const { data, isPending, content } = useQueryHandle(getExcelA);
+
+	const [selectedSheetIndex, setSelectedSheetIndex] = useState(0);
+
+	const excludedColumns = [
+		"id",
+		"create_by",
+		"create_date",
+		"update_by",
+		"update_date",
+	];
+
+	if (isPending) {
+		return content;
+	}
+
+	return (
+		<ExcelViewer
+			original_sheets={getExcelData(
+				excludeDataColumn(getBonusExcel(data), excludedColumns),
+			)}
+			original_data={getBonusExcel(data)}
+			filter_component={<></>}
+			selectedSheetIndex={selectedSheetIndex}
+			setSelectedSheetIndex={setSelectedSheetIndex}
+		/>
+	);
 }
+
+export const getStaticProps = async ({ locale }: { locale: string }) => {
+	return {
+		props: {
+			...(await serverSideTranslations(
+				locale,
+				["common", "nav"],
+				i18n,
+				locales,
+			)),
+		},
+	};
+};
